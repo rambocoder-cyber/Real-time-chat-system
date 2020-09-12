cc87751a5eb29a53449106b566c257cef0422175
Hitesh <hiteshslipknot@gmail.com>
2020-09-12 17:54:39 +0545

    Initial commit

---
+++ NodeServer/index.js
@@ -0,0 +1,20 @@
+const io = require('socket.io')(3000)
+
+const users ={};
+
+io.on('connection',socket =>{
+    socket.on('new-user-joined',name =>{
+        users[socket.id] = name;
+        console.log("New user", name);
+        socket.broadcast.emit('user-joined',name);
+    });
+
+    socket.on('send',message =>{
+        socket.broadcast.emit('recieve',{message : message , name : users[socket.id]});
+    });
+    
+    socket.on('disconnect',message =>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    });
+})
